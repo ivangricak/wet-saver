dddd60bbdd2a911902f20b54f19d008649506bee
Ivan <gricakivan894@gmail.com>
2025-11-28 17:59:36 +0100

    update in main

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,14 +19,11 @@
     <section class="blogPage"> <!-- -->
         <div class="blogBlock">
             <div class="blogBlockMain"> <!-- ширина 800px висота 500px -->
-                <div class="blogBlockMainLeft frame"> <!-- ширина 50% висота 100% -->
+                <div class="blogBlockMainLeft frame"> 
                 </div>
                 <div class="blogBlockMainRight"> <!-- ширина 50% висота 100% -->
                 <h2 class="mt-4 ms-4 mb-2 fw-bold">WHAT WE OFFER</h2>
                 <div class="accordion accordion-flush" id="accordionFlushExample">
-
-
-                
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="flush-headingOne">
                         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" >
