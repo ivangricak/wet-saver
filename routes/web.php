4a2eab7693de708c56f14acbd3995af9a94342e4
Ivan <gricakivan894@gmail.com>
2026-03-01 17:30:47 +0100

    add

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Request;
+// use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\Profile\ProfileController;
@@ -28,6 +29,7 @@ Route::get('/login', [LoginController::class, 'show'])->name('login.show');
 Route::post('/login', [LoginController::class, 'login'])->name('user.login');
 // routes/api.php
 Route::post('/api/login', [LoginController::class, 'apiLogin']);
+Route::get('/api/login', [LoginController::class, 'apiLogin']);
 Route::middleware('auth:sanctum')->get('/api/user', function (Request $request) {
     return $request->user();
 });
