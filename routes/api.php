c4b54e4dfcf3d5bfbe7c5cf7fbc8fb121d6c6fc5
Ivan <gricakivan894@gmail.com>
2026-03-01 20:47:30 +0100

    test api

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\ApiLoginController;
 
-Route::post('/login', [ApiLoginController::class, 'login']);
+Route::post('api/login', [ApiLoginController::class, 'login']);
 Route::middleware('auth:sanctum')->get('/users/nicks', function () {
     return \App\Models\User::pluck('nick');
 });
