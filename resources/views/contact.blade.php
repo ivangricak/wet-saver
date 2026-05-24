a0914d2d4f5f5e40911de40e78723273b67e574f
Ivan <gricakivan894@gmail.com>
2026-05-24 20:53:10 +0200

    solve problem

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-    <script type="application/ld+json">
-    {
-    "@context": "https://schema.org",
-    "@type": "SoftwareApplication",
-    "name": "Nexora",
-    "applicationCategory": "ProductivityApplication",
-    "operatingSystem": "Web",
-    "description": "Nexora helps users save links, organize bookmarks, and access important websites quickly.",
-    "url": "https://nexora.download"
-    }
-    </script>
     <head>
+
+        @verbatim
+        <script type="application/ld+json">
+        {
+        "@context": "https://schema.org",
+        "@type": "SoftwareApplication",
+        "name": "Nexora",
+        "applicationCategory": "ProductivityApplication",
+        "operatingSystem": "Web",
+        "description": "Nexora helps users save links, organize bookmarks, and access important websites quickly.",
+        "url": "https://nexora.download"
+        }
+        </script>
+        @endverbatim
+
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="Nexora helps you save links, organize bookmarks, and access important resources instantly. Stop losing tabs and keep everything in one place." />
@@ -127,10 +131,10 @@
         <section class="hero" id="service">
             <div class="container">
                 <div class="hero-content">
-                    <h1 class="fw-bold mb-4 hero-h1">
+                    <h2 class="fw-bold mb-4 hero-h1">
                         You save useful things,<br>
                         but don’t come back to them
-                    </h1>
+                    </h2>
 
                     <div class="row">
                         <div class="col-6 feature">
