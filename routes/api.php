2e494bfaa6d5cf6730cdce3c03ee4b501347cf8a
Ivan <gricakivan894@gmail.com>
2026-06-14 13:26:37 +0200

    solve prob

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 use App\Models\User;
 use App\Models\Group;
 use App\Models\Item;
+use App\Models\DefaultGroup;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Requests\Item\StoreRequest;
