42dd9d4553a958d24c14c20ad465c4708b67ad7d
Ivan <gricakivan894@gmail.com>
2026-06-17 22:29:49 +0200

    add new th

---
+++ config/cors.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'paths' => ['api/*', 'categories', 'users/*'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'chrome-extension://onknhellkichadbomnpbmdkhfjhkgegk',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+];
